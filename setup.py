7671edc8b895da8afdc64ac82a9d9461ab8dc9cd
Klaus Bremer <bremer@bremer-media.de>
2017-04-14 11:57:34 +0200

    version change

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
 
 
 setup(name='pyxmahjongg',
-      version='0.2.2',
+      version='0.2.3',
       description='The classic UNIX xmahjongg for Python 3',
       long_description=readme(),
       entry_points={'console_scripts': [
